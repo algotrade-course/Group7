805be1ba5ab91b7c508b32741aabbb6d5824c2d4
pnguyenhere <phnguyen.pn001@gmail.com>
2025-04-17 18:02:43 +0700

    optimizing and out sample testing

--- helper.py
+++ helper.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
 
 def plot_dataset(df_train):
     plt.figure(figsize=(12, 6))
-    plt.plot(df_train.index.to_pydatetime(), df_train['price'], label='Price', color='blue')
+    plt.plot(df_train.index.to_pydatetime(),
+             df_train['price'], label='Price', color='blue')
 
     # Labeling
     plt.xlabel('Time')
@@ -15,6 +18,7 @@ def plot_dataset(df_train):
     plt.tight_layout()
     plt.show()
 
+
 def plot_performance(PnL, dates):
     plt.figure(figsize=(12, 6))
     plt.plot(dates, PnL, label="Portfolio Value", color="blue")
@@ -24,15 +28,18 @@ def plot_performance(PnL, dates):
     plt.legend()
     plt.grid(True)
     plt.xticks(rotation=45)  # Rotate x-axis for better readability
+    formatter = FuncFormatter(lambda x, _: f"{x:,.0f}")
+    plt.gca().yaxis.set_major_formatter(formatter)
     plt.tight_layout()
     plt.show()
 
+
 def compute_indicators(df, time_range, price_col="price", volume_col="quantity", save_path=None):
     df['datetime'] = pd.to_datetime(df['datetime'])
-    
+
     df_train = df[df['datetime'].dt.year.isin(time_range)].copy()
     df_train.set_index("datetime", inplace=True)
-    
+
     if not isinstance(df_train.index, pd.DatetimeIndex):
         df_train.index = pd.to_datetime(df_train.index)
 
@@ -49,12 +56,13 @@ def compute_indicators(df, time_range, price_col="price", volume_col="quantity",
     df_train["AvgVolume20"] = df_train[volume_col].rolling(window=20).mean()
 
     if save_path:
-        dir_name = os.path.dirname(save_path) if os.path.dirname(save_path) else "."
+        dir_name = os.path.dirname(
+            save_path) if os.path.dirname(save_path) else "."
 
         os.makedirs(dir_name, exist_ok=True)
 
         if not os.path.exists(save_path):
-            df_train.to_csv(save_path) 
+            df_train.to_csv(save_path)
         else:
             print(f"File '{save_path}' already exists.")
 
